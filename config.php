157bee67c79e057b0c0d2952ae9209a71c259b3f
root <root@ip-172-31-33-201.ap-south-1.compute.internal>
2019-06-26 03:53:00 +0000

    DevA first commit

--- config.php
+++ config.php
@@ -3,7 +3,8 @@
 mysql_connect("localhost","root","");
 mysql_select_db("shop");
 ?>
-#this is 1500 line of source code written by develperA
+#this is modified 1500 line of source code written by developerA
 
 
-# this is the 200 lines of code by deveper B
+
+
